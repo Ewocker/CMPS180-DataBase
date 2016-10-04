884414382692f8059cbc2bcecad9613b479e431f
Ewocker <chockerlin@gmail.com>
2016-10-04 13:28:04 -0700

    add file load_values_lab1.sql

---
+++ load_values_lab1.sql
@@ -0,0 +1,28 @@
+-- Inserting values to Stores table
+
+INSERT INTO Stores VALUES
+(100, '700 Front St, Santa Cruz', 'Jennifer Williams'),
+(200, '600 Front St, Santa Cruz', 'John Smith'),
+(300, '1700 Mission St, Santa Cruz', 'Robert Davis');
+
+-- Inserting values to Customers table
+
+INSERT INTO Customers VALUES
+(1, 'George', '401 Heller Dr, Santa Cruz', 'george@mail.com'),
+(2, 'Mary', '304 Cliff St, Santa Cruz', 'mary@mail.com'),
+(3, 'Anne', '1735 Alma St, Palo Alto', 'anne@mail.com');
+
+-- Inserting values to Products table
+
+INSERT INTO Products VALUES
+(1000, 'Iphone 7', 'Electronics', 'Apple'),
+(2000, 'Tide Liquid Laundry', 'Detergent', 'Tide'),
+(3000, 'Sprite', 'Beverage', 'The Coca-Cola Company');
+
+-- Inserting values to Sales table
+
+INSERT INTO Sales VALUES
+(200, 1, 1000, '09/30/2016', 1, 700, false),
+(100, 2, 2000, '10/01/2016', 2, 20, true),
+(300, 3, 3000, '10/01/2016', 10, 35, true);
+ 
